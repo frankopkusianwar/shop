486addabff6133c4f398b98ba673dc5aaebc3788
frankopkusianwar <okirorfrank3@gmail.com>
2020-08-09 00:32:56 +0300

    Handle signup errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :contact, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :userType, presence: true
+  validates :password, presence: true
 end
