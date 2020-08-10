1bcc834cab088c75a0bcb3eebf84c17255896372
frankopkusianwar <okirorfrank3@gmail.com>
2020-08-11 00:09:47 +0300

    Fix logout bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.
+      log_in @user
       flash[:success] = "Welcome to ZATTIX!"
       redirect_to root_path
     else
@@ -15,6 +16,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :contact, :userType, :password)
+    params.require(:user).permit(:name, :email, :contact, :userType, :password, :password_confirmation)
   end
 end
